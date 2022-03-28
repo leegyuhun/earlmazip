6e175f9a6058d98284eb252556cdb2e6dce80ea4
LGH\LeeGyuHun <rh0969@nate.com>
2022-03-28 09:08:18 +0900

    [22.03.28] Log파일 생성 추가

--- src/main/java/aptdata/earlmazip/repository/SiteInfoRepository.java
+++ src/main/java/aptdata/earlmazip/repository/SiteInfoRepository.java
@@ -2,20 +2,33 @@ package aptdata.earlmazip.repository;
 
 import aptdata.earlmazip.domain.SiteInfo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
+@Slf4j
 @RequiredArgsConstructor
 public class SiteInfoRepository {
 
     private final EntityManager em;
 
     public String findSiteInfo(String name) {
-        SiteInfo siteInfo = em.createQuery("select a from SiteInfo a where a.infoName = :name", SiteInfo.class)
+        log.info("findSiteInfo name: " + name);
+//        SiteInfo siteInfo = em.createQuery("select a from SiteInfo a where a.infoName = :name", SiteInfo.class)
+//                .setParameter("name", name)
+//                .getSingleResult();
+        List<SiteInfo> siteInfos = em.createQuery("select a from SiteInfo a where a.infoName = :name", SiteInfo.class)
                 .setParameter("name", name)
-                .getSingleResult();
-        return siteInfo.getInfoValue();
+                .getResultList();
+        if (siteInfos.size() == 0){
+            return "update: -";
+        }
+        else {
+            SiteInfo siteInfo = siteInfos.get(0);
+            return siteInfo.getInfoValue();
+        }
     }
 }
