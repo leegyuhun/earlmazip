b948111853f788ec7f784e0e38b15436063e662a
LGH\LeeGyuHun <rh0969@nate.com>
2022-03-31 19:01:11 +0900

    [22.03.31] api별 조회수 기록 추가 (미완)

---
+++ src/main/java/aptdata/earlmazip/repository/ApiCallStatRepository.java
@@ -0,0 +1,43 @@
+package aptdata.earlmazip.repository;
+
+import aptdata.earlmazip.domain.ApiCallStat;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Repository
+@RequiredArgsConstructor
+public class ApiCallStatRepository {
+
+    private final EntityManager em;
+
+    public void WriteApiCallStat(String gubn, String name) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd");
+        String date = simpleDateFormat.format(new Date());
+
+        ApiCallStat item = em.createQuery("select a from ApiCallStat a "
+                        + " where a.callDate = :date and a.apiGubn = :gubn "
+                        + " and a.apiName = :name",ApiCallStat.class)
+                .setParameter("date", date)
+                .setParameter("gubn", gubn)
+                .setParameter("name", name)
+                .getSingleResult();
+        if (item == null) {
+            item.setCallDate(date);
+            item.setCallYear(date.substring(0, 4));
+            item.setCallMonth(date.substring(4, 6));
+            item.setCallDay(date.substring(6, 8));
+            item.setApiGubn(gubn);
+            item.setApiName(name);
+            item.setCnt(1);
+            em.persist(item);
+        } else {
+            item.setCnt(item.getCnt()+1);
+            em.merge(item);
+        }
+
+    }
+}
